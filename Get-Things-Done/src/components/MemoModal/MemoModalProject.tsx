ca4418ababc24b7b10b41dbae3aaa6ec901b0fa5
terryf01d <boise@kakao.com>
2024-02-01 15:28:16 +0900

    폴더 구조 업데이트

--- Get-Things-Done/src/components/MemoModal/MemoModalProject.tsx
+++ Get-Things-Done/src/components/MemoModal/MemoModalProject.tsx
@@ -1,75 +1,87 @@
+import React, { useState } from "react";
 import styles from "./MemoModalProject.module.css";
+import projects from "../../../projectsData.tsx";
 
-const MemoModalProject = () => {
-  return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.floor}>
-          <div className={`${styles.project} ${styles.leaf}`}>
+const MemoModalProject = ({ onProjectSelect }) => {
+  const [activePath, setActivePath] = useState([]);
+
+  const handleInboxClick = () => {
+    onProjectSelect("inbox", "Inbox");
+  };
+
+  const handleProjectClick = (path, project) => {
+    setActivePath((prevPath) => {
+      const newPath =
+        path.length < prevPath.length && path.every((p, i) => p === prevPath[i])
+          ? path.slice(0, path.length - 1)
+          : path;
+      return newPath;
+    });
+    onProjectSelect(project.icon, project.name);
+  };
+
+  const renderProjects = (projects, currentPath = []) => {
+    // 최상위 floor일 때만 Inbox 항목을 추가합니다.
+    const isRootFloor = currentPath.length === 1 && currentPath[0] === "root";
+    return (
+      <div className={styles.floor} key={currentPath.join("-")}>
+        {isRootFloor && (
+          <div
+            className={`${styles.project} ${styles.leaf}`}
+            onClick={handleInboxClick}
+          >
             <i className="material-icons">inbox</i>
             <span>Inbox</span>
           </div>
-          <div className={`${styles.project} ${styles.branch}`}>
-            <i className="material-icons">folder</i>
-            <span>개인</span>
-            <i className="material-icons">chevron_right</i>
-          </div>
-          <div className={`${styles.project} ${styles.branch}`}>
-            <i className="material-icons">folder</i>
-            <span>학교</span>
-            <i className="material-icons">chevron_right</i>
-          </div>
-          <div className={`${styles.project} ${styles.leaf}`}>
-            <i className="material-icons">folder</i>
-            <span>folderName</span>
-          </div>
-        </div>
-        <div className={styles.floor}>
-          <div className={`${styles.project} ${styles.branch}`}>
-            <i className="material-icons">folder</i>
-            <span>folderName</span>
-            <i className="material-icons">chevron_right</i>
-          </div>
-          <div className={`${styles.project} ${styles.leaf}`}>
-            <i className="material-icons">folder</i>
-            <span>folderName</span>
-          </div>
-          <div className={`${styles.project} ${styles.leaf}`}>
-            <i className="material-icons">folder</i>
-            <span>folderName</span>
-          </div>
-          <div className={`${styles.project} ${styles.leaf}`}>
-            <i className="material-icons">folder</i>
-            <span>folderName</span>
-          </div>
-          <div className={`${styles.project} ${styles.leaf}`}>
-            <i className="material-icons">folder</i>
-            <span>folderName</span>
-          </div>
-          <div className={`${styles.project} ${styles.leaf}`}>
-            <i className="material-icons">folder</i>
-            <span>folderName</span>
-          </div>
-          <div className={`${styles.project} ${styles.leaf}`}>
-            <i className="material-icons">folder</i>
-            <span>folderName</span>
-          </div>
-          <div className={`${styles.project} ${styles.leaf}`}>
-            <i className="material-icons">folder</i>
-            <span>folderName</span>
-          </div>
-          <div className={`${styles.project} ${styles.leaf}`}>
-            <i className="material-icons">folder</i>
-            <span>folderName</span>
-          </div>
-        </div>
-        <div className={styles.floor}></div>
-        <div className={styles.floor}></div>
-        <div className={styles.floor}></div>
-        <div className={styles.floor}></div>
+        )}
+        {projects.map((project, index) => {
+          const hasSubprojects =
+            project.items && project.items.some((item) => !item.type);
+          return (
+            <div
+              key={project.id}
+              className={`${styles.project} ${
+                hasSubprojects ? styles.branch : styles.leaf
+              }`}
+              onClick={() =>
+                handleProjectClick([...currentPath, project.id], {
+                  icon: "folder",
+                  name: project.name,
+                })
+              }
+            >
+              <i className="material-icons">folder</i>
+              <span>{project.name}</span>
+              {hasSubprojects && (
+                <i className="material-icons">chevron_right</i>
+              )}
+            </div>
+          );
+        })}
       </div>
-    </>
-  );
+    );
+  };
+
+  const renderActiveFloors = () => {
+    let currentProjects = projects;
+    const floors = [];
+
+    floors.push(renderProjects(currentProjects, ["root"]));
+
+    activePath.forEach((id, index) => {
+      const project = currentProjects.find((p) => p.id === id);
+      if (project && project.items) {
+        currentProjects = project.items.filter((item) => !item.type);
+        floors.push(
+          renderProjects(currentProjects, activePath.slice(0, index + 1))
+        );
+      }
+    });
+
+    return floors;
+  };
+
+  return <div className={styles.container}>{renderActiveFloors()}</div>;
 };
 
 export default MemoModalProject;
