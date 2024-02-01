1042cc87ecc0bd17f6d8ac12b69ebf4a550ac228
terryf01d <boise@kakao.com>
2024-02-01 16:24:50 +0900

    왜인지는 모르겠지만 기존 파일이 전부 삭제된 후 재생성 된 것으로 추정, 일단 작업 내역 보존하기 위해 커밋

--- Get-Things-Done/src/interfaces/Event.ts
+++
@@ -1,7 +0,0 @@
-import { Memo } from "./Memo";
-
-export interface Event extends Memo {
-    startDate: Date;
-    //end date 없어도 캘린더 표시 가능 (?)  
-    place: string;
-}
--- Get-Things-Done/src/interfaces/Memo.ts
+++
@@ -1,9 +0,0 @@
-export interface Memo {
-    id: number;
-    title: string;
-    note: string;
-    type: 'temp' | 'todo' | 'event';
-    startDate?: Date;
-    endDate?: Date;
-    repeat?: string;
-}
